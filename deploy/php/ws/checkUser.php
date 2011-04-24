d042ae6b6a2008b829061577a3caeb29eb43f6d4
Slimfr01 <Slimfr01@eb46ab2a-5819-4697-b4a8-b19d07fab60e>
2011-04-24 13:17:41 +0000

    Added the session system

---
+++ deploy/php/ws/checkUser.php
@@ -0,0 +1,35 @@
+﻿<?php
+session_start()
+if (isset($_GET['pubkey']) && isset($_GET['uid']))
+{
+	$pubkey = $_GET['pubkey'];
+	$uid = $_GET['uid'];
+	$app = "kube-builder";
+	$appkey = "appkey";
+	$key = md5($appkey.$pubkey);
+	$url = "http://muxxu.com/app/xml?app=".$appname."&xml=user&id=".$uid."&key=".$key;
+ 
+	if($flux = simplexml_load_file($url))
+	{
+		$_SESSION['name'] = $flux['name'];
+		$_SESSION['uid'] = $uid;
+		$_SESSION['statut'] = 1;
+		include 'connection.php';
+		$req = 'INSERT `users` (`id`,`name`,`name_low`) VALUES ("'.$uid.'","'.$flux['name'].'","'.strtolower($flux['name']).'")';
+		mysql_query($req);
+	}
+
+	else
+	{
+		$_SESSION['error'] = "xml_access";
+		$_SESSION['statut'] = 0;
+	}
+}
+
+else
+{
+$_SESSION['error'] = "undefined_variable";
+$_SESSION['statut'] = 1;
+}
+
+?>
