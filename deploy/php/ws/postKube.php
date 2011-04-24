12d6182cf90fc66031e75303584659fb6b1767f6
Slimfr01 <Slimfr01@eb46ab2a-5819-4697-b4a8-b19d07fab60e>
2011-04-24 16:14:22 +0000

    Added post and session tests for postKube.php .

--- deploy/php/ws/postKube.php
+++ deploy/php/ws/postKube.php
@@ -1,14 +1,61 @@
-<!-- 
-	Renvoyer un XML de la forme présentée ici.
+<?php 
+session_start();
+include 'connection.php';
+include 'secure.php';
+
+/* 	Renvoyer un XML de la forme présentée ici.
 	Prendre les variables POST suivantes en entrée :
 		name : nom du kube
 		kube : données binaires du kube
 	
 	Définir un ID arbitraire de retour pour chaque cas d'erreur possible et créer un
 	noeud <label> comme suit dans le fichier config.xml :
-		<label code="errorSubmitResultID"><![CDATA[Label de l'erreur.]]></label>
--->
-<?php echo "<?"; ?>xml version="1.0" encoding="UTF-8"?>
+		<label code="errorSubmitResultID"><![CDATA[Label de l'erreur.]]></label> */
+
+echo "<?"; ?>xml version="1.0" encoding="UTF-8"?>
 <root>
-	<result>0</result>
+<?php
+if (isset($_SESSION['statut']) && ($_SESSION['statut'] == 1))
+{
+	$result = 0;
+?>
+	<session statut="1" name="<?php print $_SESSION['name'] ?>" uid="<?php print $_SESSION['uid'] ?>" />
+<?php
+
+	if (isset($name) && isset ($kube))
+	{
+		$name = secure_string($_POST['name']);
+		$kube = $_POST['kube'];
+		$result = 0;
+	}
+	elseif (isset($name))
+	{
+	$result = "errorSubmitFile";
+	}
+	elseif (isset($kube))
+	{
+	$result = "errorSubmitName";
+	}
+	else
+	{
+	$result = "errorSubmitPost";
+	}
+}
+
+else
+{
+	$result = "errorSubmitSession";
+	if (isset($_SESSION['error']))
+		$error = $_SESSION['error'];
+	else
+		$error = "unknown";
+?>
+
+	<session statut="0" error="<?php print $error ?>" />
+
+<?php
+}
+?>
+	<result><?php print $result ?></result>
+
 </root>
