ffcafb3d5026978ef7a6749d47681aa2e6869854
Slimfr01 <Slimfr01@eb46ab2a-5819-4697-b4a8-b19d07fab60e>
2011-04-24 02:48:30 +0000

    Db Updated with the mains tables. First version of getKubes.php

---
+++ deploy/php/ws/connection.php
@@ -0,0 +1,4 @@
+<?php
+  mysql_connect("localhost", "name", "pass" );
+  mysql_select_db('kubebuilder'); 
+?>
--- deploy/php/ws/getKubes.php
+++ deploy/php/ws/getKubes.php
@@ -1,20 +1,48 @@
-<!-- 
-	Renvoyer un XML de la forme présentée ici.
+<?php
+/* 	Renvoyer un XML de la forme présentée ici.
 	Trier les résultats par nombre de votes décroissant, et en cas d'égalité mettre les plus récents en premier (pour laisser couler les dinosaures)
 	Prendre les variables POST suivantes en entrée :
 		start : premier paramètre du LIMIT (index de début)
-		length : nombre d'items à récupérer (pense à le limite à quelque chose comme 100 en dur pour éviter que des malins nous fassent des select all en bidouillant les requêtes)
--->
-<?php echo "<?"; ?>xml version="1.0" encoding="UTF-8"?>
+		length : nombre d'items à récupérer (pense à le limite à quelque chose comme 100 en dur pour éviter que des malins nous fassent des select all en bidouillant les requêtes) */
+
+		
+// Vérification des variables envoyées en POST 
+
+if (isset($_POST['start']) && isset($_POST['length']))
+{
+	$start = floor(intval($_POST['start']));
+	$length = floor(intval($_POST['length']));
+
+	if ($length > 100 || $length <= 0)
+	{
+		$length = 50;
+	}
+}
+
+else
+{
+	$start = 0;
+	$length = 50;
+}
+
+// Connection Mysql et récupération de la liste des kubes
+
+include 'connection.php';
+$req = "SELECT * FROM kubes ORDER BY `kubes`.`score`  DESC, `kubes`.`date`  DESC LIMIT ".$start.",".$length;
+$kubes = mysql_query($req);
+echo "<?"; ?>xml version="1.0" encoding="UTF-8"?>
 <root>
-	<kubes>
-		<kube id="1" uid="89" pseudo="Durss" date="1303587982" votes="95" />
-		<kube id="2" uid="89" pseudo="Durss" date="1303587982" votes="92" />
-		<kube id="3" uid="89" pseudo="Durss" date="1303587982" votes="80" />
-		<kube id="4" uid="89" pseudo="Durss" date="1303587982" votes="75" />
-		<kube id="5" uid="89" pseudo="Durss" date="1303587982" votes="73" />
-		<kube id="6" uid="89" pseudo="Durss" date="1303587982" votes="71" />
-		<kube id="7" uid="89" pseudo="Durss" date="1303587982" votes="53" />
-		<kube id="8" uid="89" pseudo="Durss" date="1303587982" votes="41" />
+	<kubes>	
+
+<?php
+while ($kube = mysql_fetch_assoc($kubes))
+{
+	$req = "SELECT name FROM users WHERE id=".$kube['uid'];
+	$user = mysql_fetch_assoc(mysql_query($req));
+?>
+<kube id="<?php print $kube['id'] ?>" uid="<?php print $kube['uid'] ?>" name="<?php print $kube['name'] ?> pseudo="<?php print $user['name'] ?>" date="<?php print $kube['date'] ?>" votes="<?php print $kube['score'] ?>" />
+<?php
+}
+?>	
 	</kubes>
 </root>
