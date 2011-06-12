2bf15c588e6e4eea5a5ccf72a35c95ef60545214
François Dursus <durss@free.fr>
2011-06-12 15:41:33 +0000

    Quick fix of the top-left pixel that was unclickable on the editor. Max cubes per day was fucked... 30000 instead of 3. \o/

--- deploy/php/constants.php
+++ deploy/php/constants.php
@@ -10,6 +10,6 @@
 	define("MAX_VOTES_PER_DAY", 3);
 	define("ONE_VOTE_PER_KUBE", true);
 	define("REPORTS_TO_HIDE_A_KUBE", 10);
-	define("MAX_KUBES_CREATION_PER_DAY", 30000);
+	define("MAX_KUBES_CREATION_PER_DAY", 3);
 	define("WRONG_REPORTS_BEFORE_LOCK", 3);
 ?>
